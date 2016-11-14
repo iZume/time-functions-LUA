b672cacca3014fcd9e287288c71431638ae51b9d
iZume <adler32mail@gmail.com>
2016-11-14 11:41:05 -0600

    vfcxdvf
    sdasd

--- server.lua
+++ server.lua
@@ -72,24 +72,16 @@ end
 function dateToUnix(day, month, year) 
     local x = 0;
 
-    for i=baseYear,year do
-        local x_year = (60 * 60 * 24) * 365;
-        
-        x = x + x_year;
-        if(IsLeapYear(i)) then 
-            local x_leap_year = (60 * 60) * 24;
-
-            x = x + x_leap_year;
-        end
+    for i=baseYear,year-1 do
+        x = x + (60 * 60 * 24 * getYearDays(i));
     end 
 
-    for i=1,month do
-        local x_month = getMonthDays(month, year) * 86400;
-        x = x + x_month;
+    for i=1,month-1 do
+        x = x + (getMonthDays(month, year) * 86400);
     end
 
-    local x_now = day * ((60 * 60) * 24);
-    x = x + x_now; 
+    x = x + ( (day + 4) * 86400 ); 
+
     return x; 
 end
 
@@ -115,32 +107,5 @@ function IsLeapYear(year)
 end
 
 function getDateInAdd(_day, _month, _year, days, months, years) 
-    return unixToDate(dateToUnix(_day, _month, _year) + ((days * (60 * 60 * 24)) + (months * (60 * 60 * 24 * 30)) + (years * (60 * 60 * 24 * 30 * 12)))); 
+    return unixToDate(dateToUnix(_day, _month, _year) + (   (days * 86400) + (months * 2592000) + (years * 31104000)   )); 
 end
-
---[[
-
-    Examples
-    
-addEventHandler("onResourceStart", root, function()
-    outputServerLog(getWeekDay(13, 11, 2016));
-
-    outputServerLog(getMonthDays(2, 2016))
-
-    outputServerLog(getMonthName(12))
-
-    outputServerLog(getSpentsDaysInYear(13, 11, 2016))
-
-    local now = dateToUnix(13, 11, 2016);
-
-    outputServerLog(string.format("%18.0f", now))
-
-    local day, month, year = unixToDate(now);
-    
-    outputServerLog("Today is " .. getWeekDay(day, month, year))
-
-    local d, m, y = getDateInAdd(13, 11, 2016, 2, 0, 0);
-
-    outputServerLog(getWeekDay(d, m, y))
-
-end)]]
